06352f0315f39342f7a4eef5cdfcce3cbb2f1377
Alex <104495043+AA-4444@users.noreply.github.com>
2025-03-14 13:21:54 +0100

    initial commit

---
+++ Documents/Booking-App/Booking-App/Booking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booking_AppApp.swift
+//  Booking-App
+//
+//  Created by Алексей Зарицький on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Booking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Documents/Booking-App/Booking-AppTests/Booking_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Booking_AppTests.swift
+//  Booking-AppTests
+//
+//  Created by Алексей Зарицький on 3/14/25.
+//
+
+import Testing
+@testable import Booking_App
+
+struct Booking_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
